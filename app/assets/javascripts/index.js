e39076268f9cd2a54f9ee15e238e44fae1470444
Enrico Schlag <enricoschlag@googlemail.com>
2014-10-08 09:51:23 +0200

    added transfer info instead alert insteadt of weird mail text

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -11,22 +11,23 @@ $(function () {
   });
 
 
+  // Show transfer info
+  // ---------------------------------------------------------------------------
+
+  $('#show-transfer-info').click(function(e) {
+    e.preventDefault();
+    $('#donate-transfer-info').slideToggle();
+  });
+
+
   // Contact/Donate link
   // ---------------------------------------------------------------------------
   $('#contact-mail, #donate-mail').click(function (e) {
     var isDonate = this.id == 'donate-mail',
         address = '@' + 'gmail.com', // weird concatenation stuff for making spam bots' lives a little harder
-        subject = 'HTWK2iCal' + (isDonate ? ' unterstützen' : '');
+        subject = 'HTWK2iCal' + (isDonate ? ' Kontodaten' : '');
     address = 'mailto:enricoschlag' + address + '?subject=' +
               encodeURIComponent(subject);
-    
-    if (isDonate) {
-      var body = 'Lieber HTWK2iCal-Entwickler, bitte teile mir deine ' +
-                 'Kontodaten mit, damit ich etwas zum Bestehen von HTWK2iCal ' +
-                 'beisteuern kann. Kthxbye!';
-      address += '&body=' + encodeURIComponent(body);
-    }
-      
     $(this).attr('href', address);
   });
 });
