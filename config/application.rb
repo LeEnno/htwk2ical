9a9fb851a054efb41e3d8ef9505f16d28899f025
Enrico Schlag <enricoschlag@googlemail.com>
2015-03-31 17:51:37 +0200

    updated config + navi label for ss15

--- config/application.rb
+++ config/application.rb
@@ -73,22 +73,22 @@ module Htwk2ical
     config.is_maintenance = true
 
     # calendars created before this date will be told to update
-    config.latest_valid_date = Time.new(2014, 10, 4, 11, 0)
+    config.latest_valid_date = Time.new(2015, 3, 31, 17, 0)
 
     # date and week when semester started
-    config.start_date = Time.new(2014, 9, 1)
-    config.start_week = 36
+    config.start_date = Time.new(2015, 2, 23)
+    config.start_week = 9
 
     # path to XML files for subjects and courses
-    config.all_subjects_xml_url = 'http://stundenplan.htwk-leipzig.de:8080/stundenplan/semgrp/semgrp_ws.xml'
-    config.all_courses_xml_url  = 'http://stundenplan.htwk-leipzig.de:8080/stundenplan/modul/modul_ws.xml'
+    config.all_subjects_xml_url = 'http://stundenplan.htwk-leipzig.de:8080/stundenplan/semgrp/semgrp_ss.xml'
+    config.all_courses_xml_url  = 'http://stundenplan.htwk-leipzig.de:8080/stundenplan/modul/modul_ss.xml'
     
     # ID of XML node in 'all_courses_xml_url' that contains all studium generale modules
-    config.studium_generale_fakultaet_id  = '%23SPLUS0AC5B0'
+    config.studium_generale_fakultaet_id  = '%23SPLUS905495'
 
     # base path for single schedule per subject and studium generale
-    config.single_subjects_html_url   = 'http://stundenplan.htwk-leipzig.de:8080/ws/Berichte/Text-Listen;Studenten-Sets;name;###SLUG###?template=UNEinzelGru&weeks=36-61'
-    config.studium_generales_html_url = 'http://stundenplan.htwk-leipzig.de:8080/ws/Berichte/Text-Listen;Module;id;###SLUG###?template=UNEinzelLV&weeks=36-61'
+    config.single_subjects_html_url   = 'http://stundenplan.htwk-leipzig.de:8080/ss/Berichte/Text-Listen;Studenten-Sets;name;###SLUG###?template=UNEinzelGru&weeks=9-36'
+    config.studium_generales_html_url = 'http://stundenplan.htwk-leipzig.de:8080/ss/Berichte/Text-Listen;Module;id;###SLUG###?template=UNEinzelLV&weeks=9-36'
 
   end
 end
