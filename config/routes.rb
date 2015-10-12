e8b85344e20c4ad03def0caeba13d17dc46e371f
LeEnno <enricoschlag@googlemail.com>
2015-10-12 08:40:56 +0200

    another try for constraining calendar#get

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Htwk2ical::Application.routes.draw do
     match "calendar/link"                   => "calendar#get_link", :as => 'calendar_link'
   end
 
-  match "/subjects"          => "calendar#get_subjects", :as => 'subjects', :defaults => {:format => 'json'}
-  match "/studium_generales" => "calendar#get_studium_generales", :as => 'studium_generales', :defaults => {:format => 'json'}
-  match ":calendar_secret"   => "calendar#get", :constraints => {:calendar_secret => /\w{8}/}, :as => 'calender_get', :defaults => {:format => 'ics'}
+  match "/subjects"          => "calendar#get_subjects", :defaults => {:format => 'json'}, :as => 'subjects'
+  match "/studium_generales" => "calendar#get_studium_generales", :defaults => {:format => 'json'}, :as => 'studium_generales'
+  match ":calendar_secret"   => "calendar#get", :constraints => {:calendar_secret => /\w{8}/}, :constraints => {:format => 'ics'}, :defaults => {:format => 'ics'}, :as => 'calender_get'
 end
