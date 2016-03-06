961d7ce87bd1aadc20b71eb30a61b4f7f6678e09
LeEnno <enricoschlag@googlemail.com>
2016-03-06 11:56:46 +0100

    maintenance on

--- config/application.rb
+++ config/application.rb
@@ -70,25 +70,24 @@ module Htwk2ical
     # --------------------------------------------------------------------------
 
     # maintenanace mode
-    config.is_maintenance = false
+    config.is_maintenance = true
 
     # calendars created before this date will be told to update
-    config.latest_valid_date = Time.new(2015, 9, 28, 19, 0)
+    config.latest_valid_date = Time.new(2016, 3, 6, 11)
 
     # date and week when semester started
-    config.start_date = Time.new(2015, 8, 31)
-    config.start_week = 36
+    config.start_date = Time.new(2016, 3, 28)
+    config.start_week = 13
 
     # path to XML files for subjects and courses
-    config.all_subjects_xml_url = 'http://stundenplan.htwk-leipzig.de:8080/stundenplan/semgrp/semgrp_ws.xml'
-    config.all_courses_xml_url  = 'http://stundenplan.htwk-leipzig.de:8080/stundenplan/modul/modul_ws.xml'
+    config.all_subjects_xml_url = 'http://stundenplan.htwk-leipzig.de:8080/stundenplan/semgrp/semgrp_ss.xml'
+    config.all_courses_xml_url  = 'http://stundenplan.htwk-leipzig.de:8080/stundenplan/modul/modul_ss.xml'
     
     # ID of XML node in 'all_courses_xml_url' that contains all studium generale modules
-    config.studium_generale_fakultaet_id  = '%23SPLUS0AC5B0'
+    config.studium_generale_fakultaet_id  = '%23SPLUS905495'
 
     # base path for single schedule per subject and studium generale
-    config.single_subjects_html_url   = 'http://stundenplan.htwk-leipzig.de:8080/ws/Berichte/Text-Listen;Studenten-Sets;name;###SLUG###?template=UNEinzelGru&weeks=36-62'
-    config.studium_generales_html_url = 'http://stundenplan.htwk-leipzig.de:8080/ws/Berichte/Text-Listen;Module;id;###SLUG###?template=UNEinzelLV&weeks=36-62'
-
+    config.single_subjects_html_url   = 'http://stundenplan.htwk-leipzig.de:8080/ss/Berichte/Text-Listen;Studenten-Sets;name;###SLUG###?template=UNEinzelGru&weeks=9-35'
+    config.studium_generales_html_url = 'http://stundenplan.htwk-leipzig.de:8080/ss/Berichte/Text-Listen;Module;id;###SLUG###?template=UNEinzelLV&weeks=9-35'
   end
 end
