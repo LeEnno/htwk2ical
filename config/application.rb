44cb995376f526eb2cc8f580942cf7fd5b9ee88a
LeEnno <enricoschlag@googlemail.com>
2016-04-01 08:25:24 +0200

    end maintenance mode and enter new term

--- config/application.rb
+++ config/application.rb
@@ -70,7 +70,7 @@ module Htwk2ical
     # --------------------------------------------------------------------------
 
     # maintenanace mode
-    config.is_maintenance = true
+    config.is_maintenance = false
 
     # calendars created before this date will be told to update
     config.latest_valid_date = Time.new(2016, 3, 6, 11)
