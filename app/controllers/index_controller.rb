be6991c1202287f1da9301adf011f52e41e8f1c8
Enno <enricoherrmann@posteo.de>
2022-10-15 14:49:07 +0200

    adjusted required donation amount

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -28,8 +28,8 @@ class IndexController < ApplicationController
 
   def _set_donate_vars
     @amount_current  = SubjectCache.find_by(key: 'donate_amount').value.to_f
-    @amount_max      = 50
+    @amount_max      = 100
     @amount_percent  = (100 * @amount_current / @amount_max).round(2)
-    @amount_heroku   = 9
+    @amount_heroku   = 16
   end
 end
