174e1eb8b4d8629f9fc5c018058eda0fcd52785c
Enno <enricoherrmann@posteo.de>
2022-11-05 11:48:05 +0100

    fix mutagen sync problems for sprockets cache on macOS

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,10 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  if ENV['SPROCKETS_CACHE_PATH']
+    config.assets.configure do |env|
+      env.cache = Sprockets::Cache::FileStore.new(ENV['SPROCKETS_CACHE_PATH'])
+    end
+  end
 end
