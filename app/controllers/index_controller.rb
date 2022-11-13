b0dd9e8f897fd16af1abebde71d1e58680046c5d
Enno <enricoherrmann@posteo.de>
2022-11-13 13:54:18 +0100

    donation amount reflects heroku eco dynos pricing

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -28,8 +28,8 @@ class IndexController < ApplicationController
 
   def _set_donate_vars
     @amount_current  = SubjectCache.find_by(key: 'donate_amount').value.to_f
-    @amount_max      = 100
+    @amount_max      = 80
     @amount_percent  = (100 * @amount_current / @amount_max).round(2)
-    @amount_heroku   = 16
+    @amount_heroku   = 14
   end
 end
