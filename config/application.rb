496b3171506215b7b5efb4babc445b638e5b8b71
Enrico Herrmann <enricoherrmann@posteo.de>
2023-09-15 20:13:25 +0200

    removed donation call

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ module Htwk2ical
 
     # maintenanace mode
     config.is_maintenance     = true
-    config.show_donation_call = true
+    config.show_donation_call = false
 
     # calendars created before this date will be told to update
     config.latest_valid_date = Time.new(2023, 4, 2, 20, 39)
